e2556ea306055624764b2274153143fcc85ae559
Joey Turczak <joeyturczak@gmail.com>
2022-02-11 22:17:35 -0800

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
       <div id="education" class="content-item">
         <div class="vertical-center">
           <h2>Education</h2>
-          <?php foreach($json_data['education'] AS $education) { ?>
+          <!-- <?php foreach($json_data['education'] AS $education) { ?>
               <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                 <div class="flex-grow-1">
                   <h3 class="mb-0"><?php echo $education['school']; ?></h3>
@@ -56,7 +56,7 @@
                   <span class="text-primary"><?php echo $education['period']; ?></span>
                 </div>
               </div>
-          <?php  } ?>
+          <?php  } ?> -->
         </div>
       </div>
       <hr>
